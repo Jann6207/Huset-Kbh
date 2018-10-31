dafe545b24273c87a99bf57e7090505483b0de66
Magnus Vagn Jensen <magnusvagnjensen@Magnuss-MBP-3.domain_not_set.invalid>
2018-10-31 15:54:50 +0100

    billede atrributes updateret

--- script/bastard_cafe.js
+++ script/bastard_cafe.js
@@ -8,7 +8,17 @@
             let jsonObject = await fetch("http://jakobfalkenberg.dk/kea/2sem/tema7/huset/wordpress/wp-json/wp/v2/bastard_cafe/172");
             page = await jsonObject.json();
 
-            //indsætter i valgt html class
+            //indsætter content i valgt html class
+
             document.querySelector(".bastard_content").innerHTML = page.content.rendered;
+
+            //Billede url
             document.querySelector("img").src = page.acf.billede.url;
+            //alt tekst url
+            document.querySelector("img").alt = page.acf.billede.alt;
+            //title
+            document.querySelector("img").title = page.acf.billede.title;
+
+
+
         }
