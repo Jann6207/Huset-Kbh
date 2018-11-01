ebe12e6fcb010620479cd4d4789339c85d2518fd
Magnus Vagn Jensen <magnusvagnjensen@Magnuss-MBP-3.domain_not_set.invalid>
2018-11-01 12:04:18 +0100

    generelle spørgsmål method update

--- script/generelle_sporgsmaal.js
+++ script/generelle_sporgsmaal.js
@@ -5,30 +5,37 @@
         //henter json fil fra WP url
         async function getJson() {
 
-            let jsonObject = await fetch("http://jakobfalkenberg.dk/kea/2sem/tema7/huset/wordpress/wp-json/wp/v2/generelle_sporgsmal/209");
+            let jsonObject = await fetch("http://jakobfalkenberg.dk/kea/2sem/tema7/huset/wordpress/wp-json/wp/v2/generelle_sporgsmal");
             page = await jsonObject.json();
 
-//            indsæt header info
+            //post find variabler lavers for hver post ID
 
-            document.querySelector(".site_header").textContent = page.acf.header;
+            //post med id 209
+            let post209 = page.find(x => x.id == '209');
+
+
+
+            //indsæt header info
+
+            document.querySelector(".site_header").textContent = post209.acf.header;
 
             //indsæt tag-line info
 
-            document.querySelector(".tag_line").textContent = page.acf.tagline;
+            document.querySelector(".tag_line").textContent = post209.acf.tagline;
 
             //indsætter content i valgt html class
 
-            document.querySelector(".generelle_sporgsmaal_content").innerHTML = page.content.rendered;
+            document.querySelector(".generelle_sporgsmaal_content").innerHTML = post209.content.rendered;
 
             //Billede url
-            document.querySelector("img").src = page.acf.billede.url;
+            document.querySelector("img").src = post209.acf.billede.url;
             //alt tekst url
-            document.querySelector("img").alt = page.acf.billede.alt;
+            document.querySelector("img").alt = post209.acf.billede.alt;
             //title
-            document.querySelector("img").title = page.acf.billede.title;
+            document.querySelector("img").title = post209.acf.billede.title;
 
             //Video
-            document.querySelector(".video_container").innerHTML = page.acf.video;
+            document.querySelector(".video_container").innerHTML = post209.acf.video;
 
 
         }
