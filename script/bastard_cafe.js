98355e54e5b2f7384dd1a14f67bf7f8ba0b6c16d
Magnus Vagn Jensen <magnusvagnjensen@Magnuss-MBP-3.domain_not_set.invalid>
2018-11-01 12:36:43 +0100

    bastard done

--- script/bastard_cafe.js
+++ script/bastard_cafe.js
@@ -5,30 +5,41 @@
         //henter json fil fra WP url
         async function getJson() {
 
-            let jsonObject = await fetch("http://jakobfalkenberg.dk/kea/2sem/tema7/huset/wordpress/wp-json/wp/v2/bastard_cafe/172");
+            let jsonObject = await fetch("http://jakobfalkenberg.dk/kea/2sem/tema7/huset/wordpress/wp-json/wp/v2/bastard_cafe");
             page = await jsonObject.json();
 
-            //indsæt header info
 
-            document.querySelector(".site_header").textContent = page.acf.header;
+post172Output();
+
+        }
+
+function post172Output (){
+
+            //post med id 209
+            let post172 = page.find(x => x.id == '172');
+
+
+        //indsæt header info
+
+            document.querySelector(".site_header").textContent = post172.acf.header;
 
             //indsæt tag-line info
 
-            document.querySelector(".tag_line").textContent = page.acf.tagline;
+            document.querySelector(".tag_line").textContent = post172.acf.tagline;
 
             //indsætter content i valgt html class
 
-            document.querySelector(".bastard_content").innerHTML = page.content.rendered;
+            document.querySelector(".bastard_content").innerHTML = post172.content.rendered;
 
             //Billede url
-            document.querySelector("img").src = page.acf.billede.url;
+            document.querySelector("img").src = post172.acf.billede.url;
             //alt tekst url
-            document.querySelector("img").alt = page.acf.billede.alt;
+            document.querySelector("img").alt = post172.acf.billede.alt;
             //title
-            document.querySelector("img").title = page.acf.billede.title;
+            document.querySelector("img").title = post172.acf.billede.title;
 
             //Video
-            document.querySelector(".video_container").innerHTML = page.acf.video;
+            document.querySelector(".video_container").innerHTML = post172.acf.video;
 
 
-        }
+}
