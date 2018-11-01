e37be37aa0078d527a10e3891aeb1eba4b9b4263
jako2889 <jako2889@stud.kea.dk>
2018-11-01 12:16:13 +0100

    kultur historie rettet

--- script/kultur_historie.js
+++ script/kultur_historie.js
@@ -5,30 +5,40 @@
         //henter json fil fra WP url
         async function getJson() {
 
-            let jsonObject = await fetch("http://jakobfalkenberg.dk/kea/2sem/tema7/huset/wordpress/wp-json/wp/v2/kultur_historie/196");
+            let jsonObject = await fetch("http://jakobfalkenberg.dk/kea/2sem/tema7/huset/wordpress/wp-json/wp/v2/kultur_historie");
             page = await jsonObject.json();
 
-            //indsæt header info
+            post196Output();
 
-            document.querySelector(".site_header").textContent = page.acf.header;
-
-            //indsæt tag-line info
-
-            document.querySelector(".tag_line").textContent = page.acf.tagline;
-
-            //indsætter content i valgt html class
-
-            document.querySelector(".kultur_historie_content").innerHTML = page.content.rendered;
-
-            //Billede url
-            document.querySelector("img").src = page.acf.billede.url;
-            //alt tekst url
-            document.querySelector("img").alt = page.acf.billede.alt;
-            //title
-            document.querySelector("img").title = page.acf.billede.title;
-
-            //Video
-            document.querySelector(".video_container").innerHTML = page.acf.video;
 
 
         }
+
+
+function post196Output() {
+    console.log("test");
+
+    let post196 = page.find(x => x.id == '196');
+
+                //indsæt header info
+
+            document.querySelector(".site_header").textContent = post196.acf.header;
+
+            //indsæt tag-line info
+
+            document.querySelector(".tag_line").textContent = post196.acf.tagline;
+
+            //indsætter content i valgt html class
+
+            document.querySelector(".kultur_historie_content").innerHTML = post196.content.rendered;
+
+            //Billede url
+            document.querySelector("img").src = post196.acf.billede.url;
+            //alt tekst url
+            document.querySelector("img").alt = post196.acf.billede.alt;
+            //title
+            document.querySelector("img").title = post196.acf.billede.title;
+
+            //Video
+            document.querySelector(".video_container").innerHTML = post196.acf.video;
+}
