173b74658dd7c272bb817b5f80b369bacd40d1be
jako2889 <jako2889@stud.kea.dk>
2018-11-01 12:42:40 +0100

    kontakt_os

--- script/kontakt_os.js
+++ script/kontakt_os.js
@@ -5,30 +5,37 @@
         //henter json fil fra WP url
         async function getJson() {
 
-            let jsonObject = await fetch("http://jakobfalkenberg.dk/kea/2sem/tema7/huset/wordpress/wp-json/wp/v2/kontakt_os/203");
+            let jsonObject = await fetch("http://jakobfalkenberg.dk/kea/2sem/tema7/huset/wordpress/wp-json/wp/v2/kontakt_os");
             page = await jsonObject.json();
 
-            //indsæt header info
 
-            document.querySelector(".site_header").textContent = page.acf.header;
+post203Output();
+        }
+
+function post203Output() {
+
+    let post203 = page.find(x => x.id == '203');
+
+                //indsæt header info
+
+            document.querySelector(".site_header").textContent = post203.acf.header;
 
             //indsæt tag-line info
 
-            document.querySelector(".tag_line").textContent = page.acf.tagline;
+            document.querySelector(".tag_line").textContent = post203.acf.tagline;
 
             //indsætter content i valgt html class
 
-            document.querySelector(".kontakt_os_content").innerHTML = page.content.rendered;
+            document.querySelector(".kontakt_os_content").innerHTML = post203.content.rendered;
 
             //Billede url
-            document.querySelector("img").src = page.acf.billede.url;
+            document.querySelector("img").src = post203.acf.billede.url;
             //alt tekst url
-            document.querySelector("img").alt = page.acf.billede.alt;
+            document.querySelector("img").alt = post203.acf.billede.alt;
             //title
-            document.querySelector("img").title = page.acf.billede.title;
+            document.querySelector("img").title = post203.acf.billede.title;
 
             //Video
-            document.querySelector(".video_container").innerHTML = page.acf.video;
+            document.querySelector(".video_container").innerHTML = post203.acf.video;
 
-
-        }
+}
