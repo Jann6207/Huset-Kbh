1fe43e7c6859872743fc00542cb0a1d3ff651f08
Magnus Vagn Jensen <magnusvagnjensen@Magnuss-MBP-3.domain_not_set.invalid>
2018-11-05 20:25:01 +0100

    nav bar implementeret i kalender

---
+++ script/java.js
@@ -0,0 +1,17 @@
+$(window).on("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+    $(".menubutton").on("click", trykPaaMenubutton);
+}
+
+function trykPaaMenubutton() {
+    console.log("Tryk paa menubutton");
+    $("nav").slideToggle(".navbar2");
+    $(".menubutton").toggleClass("kryds");
+
+
+
+
+}
+
