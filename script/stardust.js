856bb496fa87aa21da46824dcb778fb0ef186f6c
Magnus Vagn Jensen <magnusvagnjensen@Magnuss-MBP-3.domain_not_set.invalid>
2018-11-06 13:27:40 +0100

    Balsalen og Salon K tilføjet til nav

--- script/stardust.js
+++ script/stardust.js
@@ -31,11 +31,11 @@ function post191Output (){
             document.querySelector(".stardust_content").innerHTML = post191.content.rendered;
 
             //Billede url
-            document.querySelector("img").src = post191.acf.billede.url;
+            document.querySelector(".content_image").src = post191.acf.billede.url;
             //alt tekst url
-            document.querySelector("img").alt = post191.acf.billede.alt;
+            document.querySelector(".content_image").alt = post191.acf.billede.alt;
             //title
-            document.querySelector("img").title = post191.acf.billede.title;
+            document.querySelector(".content_image").title = post191.acf.billede.title;
 
             //Video
             document.querySelector(".video_container").innerHTML = post191.acf.video;
