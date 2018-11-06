fa874bb98746bf48c6d97974027d29345949ed11
Magnus Vagn Jensen <magnusvagnjensen@Magnuss-MBP-3.domain_not_set.invalid>
2018-11-06 12:42:44 +0100

    evoo + main . 10% content image

--- script/evoo_street_kitchen.js
+++ script/evoo_street_kitchen.js
@@ -30,11 +30,11 @@ function post186Output (){
             document.querySelector(".evoo_street_kitchen_content").innerHTML = post186.content.rendered;
 
             //Billede url
-            document.querySelector("img").src = post186.acf.billede.url;
+            document.querySelector(".content_image").src = post186.acf.billede.url;
             //alt tekst url
-            document.querySelector("img").alt = post186.acf.billede.alt;
+            document.querySelector(".content_image").alt = post186.acf.billede.alt;
             //title
-            document.querySelector("img").title = post186.acf.billede.title;
+            document.querySelector(".content_image").title = post186.acf.billede.title;
 
             //Video
             document.querySelector(".video_container").innerHTML = post186.acf.video;
