68096cea7e590f4c195e61ffecd900c76d6235f8
Magnus Vagn Jensen <magnusvagnjensen@Magnuss-MBP-3.domain_not_set.invalid>
2018-11-07 17:51:54 +0100

    arrangøre og samj abde

--- script/kultur_historie.js
+++ script/kultur_historie.js
@@ -9,6 +9,7 @@
             page = await jsonObject.json();
 
             post196Output();
+        sreenSize();
 
 
 
@@ -44,3 +45,79 @@ function post196Output() {
             //Video
             document.querySelector(".video_container").innerHTML = post196.acf.video;
 }
+
+ // SCREEN LISTENER ÅBNER OG LUKKER ET ELEMENT
+
+    function sreenSize() {
+        console.log("screeeen siiize");
+        if (window.matchMedia("(min-width: 1000px)").matches) {
+            /* the viewport is at least 400 pixels wide */
+        } else {
+
+            document.querySelector(".section_1").addEventListener("click", section_1_mobil);
+            document.querySelector(".section_2").addEventListener("click", section_2_mobil);
+            document.querySelector(".section_3").addEventListener("click", section_3_mobil);
+            document.querySelector(".section_4").addEventListener("click", section_4_mobil);
+            document.querySelector(".section_5").addEventListener("click", section_5_mobil);
+
+
+        }
+    }
+
+    function section_1_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_1");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+
+    function section_2_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_2");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+    function section_3_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_3");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+    function section_4_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_4");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+    function section_5_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_5");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+
