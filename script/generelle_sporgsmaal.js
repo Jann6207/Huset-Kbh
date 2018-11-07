00e46b793433a3e7ece8558fa494cf2002f15929
jako2889 <jako2889@stud.kea.dk>
2018-11-07 18:45:52 +0100

    FAQ mobil færdig

--- script/generelle_sporgsmaal.js
+++ script/generelle_sporgsmaal.js
@@ -9,6 +9,7 @@
             page = await jsonObject.json();
 
             post229Output();
+            sreenSize();
         }
 
 function post229Output (){
@@ -42,3 +43,186 @@ function post229Output (){
 
 
 }
+
+// SCREEN LISTENER ÅBNER OG LUKKER ET ELEMENT
+
+    function sreenSize() {
+        console.log("screeeen siiize");
+        if (window.matchMedia("(min-width: 1000px)").matches) {
+            /* the viewport is at least 400 pixels wide */
+        } else {
+
+            document.querySelector(".section_1").addEventListener("click", section_1_mobil);
+            document.querySelector(".section_2").addEventListener("click", section_2_mobil);
+            document.querySelector(".section_3").addEventListener("click", section_3_mobil);
+            document.querySelector(".section_4").addEventListener("click", section_4_mobil);
+            document.querySelector(".section_5").addEventListener("click", section_5_mobil);
+            document.querySelector(".section_6").addEventListener("click", section_6_mobil);
+            document.querySelector(".section_7").addEventListener("click", section_7_mobil);
+            document.querySelector(".section_8").addEventListener("click", section_8_mobil);
+            document.querySelector(".section_9").addEventListener("click", section_9_mobil);
+            document.querySelector(".section_10").addEventListener("click", section_10_mobil);
+            document.querySelector(".section_11").addEventListener("click", section_11_mobil);
+            document.querySelector(".section_12").addEventListener("click", section_12_mobil);
+            document.querySelector(".section_13").addEventListener("click", section_13_mobil);
+            document.querySelector(".section_14").addEventListener("click", section_14_mobil);
+
+
+            /* the viewport is less than 400 pixels wide */
+        }
+    }
+
+    function section_1_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_1");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+
+    function section_2_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_2");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+    function section_3_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_3");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+    function section_4_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_4");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+    function section_5_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_5");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+    function section_6_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_6");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+    function section_7_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_7");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+    function section_8_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_8");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+    function section_9_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_9");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+  function section_10_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_10");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+  function section_11_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_11");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+  function section_12_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_12");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+  function section_13_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_13");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
+
+  function section_14_mobil() {
+        console.log("HEJ");
+
+        let a = document.querySelector(".show_14");
+        if (a.style.display === "none") {
+            a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    };
