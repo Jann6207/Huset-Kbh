708fcc52b3e8a6bc10b646dc9821133c206d62a3
Jann6207 <jann6207@stud.kea.dk>
2018-11-07 17:57:24 +0100

    xenon og offentligt mobile version dropdown done

--- script/xenon_vox.js
+++ script/xenon_vox.js
@@ -11,6 +11,7 @@
 
 
             post190Output();
+           sreenSize();
         }
 
 function post190Output() {
@@ -39,3 +40,70 @@ function post190Output() {
             //Video
             document.querySelector(".video_container").innerHTML = post190.acf.video;
 }
+
+
+// SCREEN LISTENER ÅBNER OG LUKKER ET ELEMENT
+
+function sreenSize(){
+    console.log("screeeen siiize");
+    if (window.matchMedia("(min-width: 1000px)").matches) {
+  /* the viewport is at least 400 pixels wide */
+} else {
+
+    document.querySelector(".section_1").addEventListener("click", section_1_mobil);
+    document.querySelector(".section_2").addEventListener("click", section_2_mobil);
+    document.querySelector(".section_3").addEventListener("click", section_3_mobil);
+
+
+
+
+
+  /* the viewport is less than 400 pixels wide */
+}
+}
+
+    function section_1_mobil(){
+
+            console.log("HEJ");
+
+    let a = document.querySelector(".show_1");
+
+    if (a.style.display === "none") {
+        a.style.display = "block";
+    } else {
+        a.style.display = "none";
+    }
+
+        };
+
+
+ function section_2_mobil(){
+
+            console.log("HEJ");
+
+    let a = document.querySelector(".show_2");
+
+    if (a.style.display === "none") {
+        a.style.display = "block";
+    } else {
+        a.style.display = "none";
+    }
+
+        };
+
+
+ function section_3_mobil(){
+
+            console.log("HEJ");
+
+    let a = document.querySelector(".show_3");
+
+    if (a.style.display === "none") {
+        a.style.display = "block";
+    } else {
+        a.style.display = "none";
+    }
+
+        };
+
+
