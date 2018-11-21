a64a4bf704efca08dade7cfe53519749392fd92b
jako2889 <jako2889@stud.kea.dk>
2018-11-21 12:11:21 +0100

    Rettet nyheder mobil

--- mobil/mobil_script/mobil_script.js
+++ mobil/mobil_script/mobil_script.js
@@ -7,23 +7,39 @@ function sidenVises() {
 
 function trykPaaMenubutton() {
     console.log("Tryk paa menubutton");
-    $("nav").slideToggle (".navbar2");
+    $("nav").slideToggle(".navbar2");
     $(".menubutton").toggleClass("kryds");
-
-
-
-
 }
 
+/* POP UP START */
 
-/*
-function trykPaaDropdownbutton() {
-    console.log("Tryk paa dropdown_mobile");
-    $(".dropdownbutton").slideToggle(".dropdown_content_mobile");
-    $(".dropdownbutton").toggleClass("drop");
-
-
-
-
+function ShowNewsletter() {
+    setTimeout(function () {
+        document.getElementById("newsletter_container").style.visibility = "visible";
+    }, 5000);
 }
-*/
+
+$(document).ready(function () {
+    $("button").click(function () {
+        $("#newsletter_container").remove();
+    });
+});
+
+/* POP UP SLUT */
+
+/* TILBAGE TIL TOP KNAP START */
+
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('#scroll').fadeIn();
+    } else {
+        $('#scroll').fadeOut();
+    }
+});
+$('#scroll').click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+});
